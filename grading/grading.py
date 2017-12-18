703013521791bfee41ad286d88cf7ad94805b91f
Sinclair Liang <sinclairliang@gmail.com>
2017-12-18 10:29:23 -0800

    figured out how to print out values

--- grading/grading.py
+++ grading/grading.py
@@ -1,4 +1,10 @@
 import pandas as pd
-
-s = pd.Series([1,3,5,6,8])
-print(s)
+import openpyxl
+xl = ("/Users/Sinclair/Dropbox/students_submissions/Lab2StudentSubmission.xlsx")
+wb = openpyxl.load_workbook(xl)
+ws = wb.active
+sheet = wb.get_sheet_by_name('Sheet1')
+print(ws['C9'].value)
+# s = pd.ExcelFile(xl)
+# df = s.parse("Sheet1")
+# print(df.head())
