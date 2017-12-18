463e478a835608f502d5838ae7d02fdb338968c1
Sinclair Liang <sinclairliang@gmail.com>
2017-12-18 11:13:03 -0800

    output_certain_row

--- grading/grading.py
+++ grading/grading.py
@@ -1,10 +1,23 @@
 import pandas as pd
+
 import openpyxl
-xl = ("/Users/Sinclair/Dropbox/students_submissions/Lab2StudentSubmission.xlsx")
-wb = openpyxl.load_workbook(xl)
+from openpyxl.compat import range
+from openpyxl.utils import get_column_letter
+def readfile(address):
+    wb = openpyxl.load_workbook(address)
+    return wb
+
+
+wb = readfile("/Users/Sinclair/Dropbox/students_submissions/testsubmission.xlsx")
 ws = wb.active
-sheet = wb.get_sheet_by_name('Sheet1')
-print(ws['C9'].value)
-# s = pd.ExcelFile(xl)
-# df = s.parse("Sheet1")
-# print(df.head())
+# sheet = wb.get_sheet_by_name('Sheet1')
+# print(ws['C9'].value)
+
+def output_certain_row(Row,start, end):
+    for column in range(start, end):
+        coor = str(Row)+str(column)
+        print(ws[coor].value)
+    return 0
+
+output_certain_row('E',3,10)
+
