46b434c9ebf3443bf567bafb683df9a9f0579a7f
Sinclair Liang <sinclairliang@gmail.com>
2017-12-18 13:56:54 -0800

    finished assigning grades when an answer partially matched function

--- grading/grading.py
+++ grading/grading.py
@@ -9,6 +9,7 @@ def readfile(address):
 
 
 wb = readfile("/Users/Sinclair/Dropbox/students_submissions/Lab2StudentSubmission_test.xlsx")
+# wb = readfile("/Users/Sinclair/Dropbox/students_submissions/testsubmission.xlsx")
 ws = wb.active
 
 
@@ -23,16 +24,33 @@ def grant_exist(row_alphabet, assign_points):
         # print(output_certain_cell('E',x))
         this_coor_alpha = chr(ord(row_alphabet)+1)
         this_coor = this_coor_alpha + str(x)
+        student_coor = 'C' + str(x)
         if output_certain_cell(row_alphabet,x) == None:
             # ws.cell(6, x).value='X values'
             ws[this_coor] = '0'
-            print(this_coor)
+            print("student " + str(ws[student_coor].value) + " has no answer")
         else:
             ws[this_coor].value = assign_points
+
+def grant_include(row_alphabet, countain, assign_points):
+    for x in range (7,162):
+        # print(output_certain_cell('E',x))
+        this_coor_alpha = chr(ord(row_alphabet)+1)
+        this_coor = this_coor_alpha + str(x)
+        student_coor = 'C' + str(x)
+        if output_certain_cell(row_alphabet,x) == None:
+            ws[this_coor] = '0'
+            print("student " + str(ws[student_coor].value) + " has no answer")
+        elif str(countain) in str(output_certain_cell(row_alphabet,x)):
+            ws[this_coor].value = assign_points
+        else:
+            ws[this_coor] = '0'
+            print("student " + str(ws[student_coor].value) + " has a wrong answer")
 
 grant_exist('E',20)
 grant_exist('H',2)
 grant_exist('J',1)
+grant_include('L','1.1',1)
 
 wb.save('write_example.xlsx')
 
